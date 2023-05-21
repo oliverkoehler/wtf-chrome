12c34a4fa0a55976dfa99d43120864f1afaf0514
oliverkoehler <olli9622@gmail.com>
2023-05-21 22:48:35 +0200

    fix

--- src/entries/contentScript/test.js
+++
@@ -1,7 +0,0 @@
-const newDiv = document.createElement("div")
-const newContent = document.createTextNode("Test")
-newDiv.appendChild(newContent)
-newDiv.classList.add("text-2xl")
-
-const ele = document.getElementsByClassName('content-title m-bottom10')[0]
-ele.insertBefore(newDiv, ele.children[1])
